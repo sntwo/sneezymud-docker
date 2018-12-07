d8798c3cbea5ade07dc2083d829e2fc728d53480
Joshua Knapp <jbomb@Joshuas-MacBook-Pro.local>
2018-12-07 15:46:20 -0800

    Revert "working on user auth"
    better stuff incoming
    
    This reverts commit 23e5c62f71c214488447a985ae3ee1a9ce39643b.

--- web/app.py
+++ web/app.py
@@ -2,13 +2,8 @@
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
-#from flask_login import LoginManager
-import crypt
-
 
 app = Flask(__name__)
-
-
 
 #import mysql.connector as mariadb
 #mariadb_connection = mariadb.connect(host='db', user='sneezy', password='password', database='sneezy')
@@ -20,23 +15,17 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://{usr}:{passwd}@{host}/{
 
 db = SQLAlchemy(app)
 
-#login = LoginManager(app)
-#class User(UserMixin, db.Model):
+ #`zone_nr` int(11) NOT NULL,
+  #`zone_name` varchar(255) NOT NULL default '',
+  #`zone_enabled` int(11) default NULL,
+  #`bottom` int(11) default NULL,
+  #`top` int(11) default NULL,
+  #`reset_mode` int(11) default NULL,
+  #`lifespan` int(11) default NULL,
+  #`age` int(11) default NULL,
+  #`util_flag` int(11) default NULL,
+  #PRIMARY KEY  (`zone_nr`)
 
-
-class Account(db.Model):
-    account_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
-    email = db.Column(db.String(80))
-    name = db.Column(db.String(80))
-    passwd = db.Column(db.String(13))
-    birth = db.Column(db.Integer)
-    term = db.Column(db.Integer)
-    time_adjust = db.Column(db.Integer)
-    flags = db.Column(db.Integer)
-    last_logon = db.Column(db.Integer)
-
-    def __repr__(self):
-        return "<Name: {}>".format(self.name)
 
 class Zone(db.Model):
     zone_nr = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
@@ -54,9 +43,10 @@ class Zone(db.Model):
 
 @app.route("/")
 def hello():
+    #cursor.execute("SELECT zone_name FROM zone")
+    #zones = list(cursor)
     zones = Zone.query.all()
-    accounts = Account.query.all()
-    return render_template("home.html", zones= zones, accounts=accounts)
+    return render_template("home.html", zones=zones)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
